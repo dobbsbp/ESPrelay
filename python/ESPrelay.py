a141f60a8a7e033556b9341bf55c8f99eb79ebf5
Ben <dobbsbp@gmail.com>
2018-11-14 08:19:17 +0700

    Create ESPrelay.py

---
+++ python/ESPrelay.py
@@ -0,0 +1,45 @@
+#The relay/ap
+
+import machine
+import network
+
+
+#network first
+ap.config(essid='SuperMassive', authmode=network.AUTH_WPA_WPA2_PSK, password="BlackHole")
+
+#webserver second. Displays pin outputs
+pins = [machine.Pin(i, machine.Pin.IN) for i in (0, 2, 4, 5, 12, 13, 14, 15)]
+
+html = """<!DOCTYPE html>
+<html>
+    <head> <title>ESP8266 Pins</title> </head>
+    <body> <h1>ESP8266 Pins</h1>
+        <table border="1"> <tr><th>Pin</th><th>Value</th></tr> %s </table>
+    </body>
+</html>
+"""
+
+#Listening for get requests i guess
+import socket
+addr = socket.getaddrinfo('0.0.0.0', 80)[0][-1]
+
+s = socket.socket()
+s.bind(addr)
+s.listen(1)
+
+print('listening on', addr)
+
+while True:
+    cl, addr = s.accept()
+    print('client connected from', addr)
+    cl_file = cl.makefile('rwb', 0)
+    while True:
+        line = cl_file.readline()
+        if not line or line == b'\r\n':
+            break
+    rows = ['<tr><td>%s</td><td>%d</td></tr>' % (str(p), p.value()) for p in pins]
+    response = html % '\n'.join(rows)
+    cl.send(response)
+    cl.close()
+
+#omg im so lost
